99d5b18ded60bbc9e587a99d497d86a19fe33e1a
IgnacioR-Varas <81173948+IgnacioR-Varas@users.noreply.github.com>
2024-06-09 04:35:40 -0400

    Add files via upload

---
+++ snake/src/frame.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class frame extends JFrame {
+    frame(){
+    this.add( new panel());
+    this.setTitle("Snake");
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setResizable(false);
+    this.pack();
+    this.setVisible(true);
+    this.setLocationRelativeTo(null);
+    }
+}
